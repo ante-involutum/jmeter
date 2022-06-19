594dc3586477e253f04274c0805603e5eef3088f
lunz1207 <lunz1207@gmail.com>
2022-06-19 22:07:05 +0800

    fix: some bug

--- build-docker.sh
+++ build-docker.sh
@@ -1,1 +1,3 @@
 docker buildx build -f Dockerfile --platform linux/amd64 -t mx2542/anti-jmeter:1.0 . --push
+
+# ./apache-jmeter-5.4.3/bin/jmeter.sh -n -t jmx/example.jmx -l log.jtl -R jmeter-slave-0.jmeter-slave.default:1099,jmeter-slave-1.jmeter-slave.default:1099
